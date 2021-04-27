04a31f48b49a334061190a45716bb05e319e63ef
dv3xxx <dv3xxx@mail.ru>
2021-04-27 14:12:29 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+
+module.exports = {
+
+    entry: __dirname + '/src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+
+    module: {  // where we defined file patterns and their loaders
+        rules: [{
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: [
+                /node_modules/
+            ]
+        },
+        {
+            test: /\.(sass|scss)$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        },
+        {
+
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+            type: 'asset/resource',
+
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            type: 'asset/resource',
+        },
+        ]
+    },
+    plugins: [  // Array of plugins to apply to build chunk
+        new HtmlWebpackPlugin({
+            template: __dirname + "/src/public/index.html",
+            inject: 'body'
+        })
+    ],
+    devServer: {  // configuration for webpack-dev-server
+        contentBase: './src/public',  //source of static assets
+        port: 7700, // port to run dev-server
+    }
+};
