f7886c6d69805ded9f7e35e36067a0f7710ca3c2
dv3xxx <dv3xxx@mail.ru>
2021-05-05 17:00:09 +0700

    for homework

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./scss/style.scss";
-
+import $ from 'jquery';
+ 
+global.jQuery = $;
+global.$ = $;
 // document.getElementById('play').onmousemove = function(e){ 
 //     console.log('Попали')
 //    }
